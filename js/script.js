cca4f850721e041a84bdabed340e90160c6d3e72
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-01-12 23:34:44 -0300

    .

--- js/script.js
+++ js/script.js
@@ -8,6 +8,40 @@ const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 
 // Funções
+const saveTodo = (text) => {
+
+    const todo = document.createElement("div")
+    todo.classList.add("todo")
+
+    const todoTitle = document.createElement("h3")
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    const doneBtn = document.createElement("button")
+    doneBtn.classList.add("finish-todo")
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement("button")
+    editBtn.classList.add("edit-todo")
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.classList.add("remove-todo")
+deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo);
+    todoInput.value = "";
+    todoInput.focus();
+};
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide")
+    todoForm.classList.toggle("hide")
+    todoList.classList.toggle("hide")
+}
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
@@ -17,5 +51,32 @@ todoForm.addEventListener("submit", (e) => {
     const inputValue = todoInput.value
 
     if(inputValue) {
+        saveTodo(inputValue)
     }
+})
+
+
+document.addEventListener("click", (e) => {
+
+    const targeEl = e.target;
+    const parentEl = targeEl.closest("div");
+
+    if(targeEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done");
+    }
+
+    if(targeEl.classList.contains("remove-todo")) {
+        parentEl.remove();
+    }
+
+    if(targeEl.classList.contains("edit-todo")) { 
+        toggleForms();
+    }
+
+})
+
+cancelEditBtn.addEventListener("click", (e) => {
+    e.preventDefault()
+    
+    toggleForms();
 })
